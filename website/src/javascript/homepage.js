1763352ab5a219346285ae17f3fd1c15c0a84ee8
kiejaana <100525896+kyanamarckx@users.noreply.github.com>
2023-08-23 00:51:57 +0200

    update homepage

---
+++ website/src/javascript/homepage.js
@@ -0,0 +1,20 @@
+
+// ---------- Header scroll effect ----------
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+  var scrollTop = window.scrollY || document.documentElement.scrollTop;
+  var header = document.querySelector("header nav");
+  
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down, hide the header
+    header.style.transform = "translateY(-100%)";
+    header.style.transition = "transform 1s ease-in-out";
+  } else {
+    // Scrolling up, show the header
+    header.style.transform = "translateY(0)";
+    header.style.transition = "transform 1s ease-in-out";
+  }
+
+  lastScrollTop = scrollTop;
+});
