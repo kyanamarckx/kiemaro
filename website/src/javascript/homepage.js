b4ebf446709f911feb077992545872f008beb2e3
kiejaana <100525896+kyanamarckx@users.noreply.github.com>
2023-08-25 21:38:00 +0200

    update homepage

--- website/src/javascript/homepage.js
+++ website/src/javascript/homepage.js
@@ -22,30 +22,164 @@ window.addEventListener("scroll", function() {
 
 // ---------- Sections hover ----------
 var sections = document.querySelectorAll("section");
+
 var company = document.querySelector("#company");
+var companyDiv = company.querySelector("div");
 var companyP = company.querySelector("p");
 var companyButton = company.querySelector("button");
 
-company.addEventListener("mouseover", function() {
+var services = document.querySelector("#services");
+var servicesDiv = services.querySelector("div");
+var servicesP = services.querySelector("p");
+var servicesButton = services.querySelector("button");
+
+var aboutus = document.querySelector("#aboutus");
+var aboutusDiv = aboutus.querySelector("div");
+var aboutusP = aboutus.querySelector("p");
+var aboutusButton = aboutus.querySelector("button");
+
+var portfolio = document.querySelector("#portfolio");
+var portfolioDiv = portfolio.querySelector("div");
+var portfolioP = portfolio.querySelector("p");
+var portfolioButton = portfolio.querySelector("button");
+
+var contact = document.querySelector("#contact");
+var contactDiv = contact.querySelector("div");
+var contactP = contact.querySelector("p");
+var contactButton = contact.querySelector("button");
+
+
+companyDiv.addEventListener("mouseover", function() {
   companyP.style.display = "block";
   companyP.style.transition = "0.5s";
+  companyP.classList.remove("fade-out");
+  companyP.classList.add("fade-in");
   companyButton.style.display = "block";
   companyButton.style.transition = "0.5s";
+  companyButton.classList.remove("fade-out");
+  companyButton.classList.add("fade-in");
   companyP.style.overflowY = "scroll";
-  // transition delay + make the appearance a fade
   companyP.style.transitionDelay = "0.5s";
   companyP.style.opacity = "1";
-  companyButton.style.transitionDelay = "0.5s";
+  // companyButton.style.transitionDelay = "1.5s";
   companyButton.style.opacity = "1";
-
 });
 
 company.addEventListener("mouseout", function() {
-  companyP.style.display = "none";
   companyP.style.transition = "0.5s";
-  companyButton.style.display = "none";
+  companyP.classList.remove("fade-in");
+  companyP.classList.add("fade-out");
   companyButton.style.transition = "0.5s";
+  companyButton.classList.remove("fade-in");
+  companyButton.classList.add("fade-out");
   companyP.style.opacity = "0";
   companyButton.style.opacity = "0";
-  
+});
+
+servicesDiv.addEventListener("mouseover", function() {
+  servicesP.style.display = "block";
+  servicesP.style.transition = "0.5s";
+  servicesP.classList.remove("fade-out");
+  servicesP.classList.add("fade-in");
+  servicesButton.style.display = "block";
+  servicesButton.style.transition = "0.5s";
+  servicesButton.classList.remove("fade-out");
+  servicesButton.classList.add("fade-in");
+  servicesP.style.overflowY = "scroll";
+  servicesP.style.transitionDelay = "0.5s";
+  servicesP.style.opacity = "1";
+  // servicesButton.style.transitionDelay = "1.5s";
+  servicesButton.style.opacity = "1";
+});
+
+services.addEventListener("mouseout", function() {
+  servicesP.style.transition = "0.5s";
+  servicesP.classList.remove("fade-in");
+  servicesP.classList.add("fade-out");
+  servicesButton.style.transition = "0.5s";
+  servicesButton.classList.remove("fade-in");
+  servicesButton.classList.add("fade-out");
+  servicesP.style.opacity = "0";
+  servicesButton.style.opacity = "0";
+});
+
+aboutusDiv.addEventListener("mouseover", function() {
+  aboutusP.style.display = "block";
+  aboutusP.style.transition = "0.5s";
+  aboutusP.classList.remove("fade-out");
+  aboutusP.classList.add("fade-in");
+  aboutusButton.style.display = "block";
+  aboutusButton.style.transition = "0.5s";
+  aboutusButton.classList.remove("fade-out");
+  aboutusButton.classList.add("fade-in");
+  aboutusP.style.overflowY = "scroll";
+  aboutusP.style.transitionDelay = "0.5s";
+  aboutusP.style.opacity = "1";
+  // aboutusButton.style.transitionDelay = "1.5s";
+  aboutusButton.style.opacity = "1";
+});
+
+aboutus.addEventListener("mouseout", function() {
+  aboutusP.style.transition = "0.5s";
+  aboutusP.classList.remove("fade-in");
+  aboutusP.classList.add("fade-out");
+  aboutusButton.style.transition = "0.5s";
+  aboutusButton.classList.remove("fade-in");
+  aboutusButton.classList.add("fade-out");
+  aboutusP.style.opacity = "0";
+  aboutusButton.style.opacity = "0";
+});
+
+portfolioDiv.addEventListener("mouseover", function() {
+  portfolioP.style.display = "block";
+  portfolioP.style.transition = "0.5s";
+  portfolioP.classList.remove("fade-out");
+  portfolioP.classList.add("fade-in");
+  portfolioButton.style.display = "block";
+  portfolioButton.style.transition = "0.5s";
+  portfolioButton.classList.remove("fade-out");
+  portfolioButton.classList.add("fade-in");
+  portfolioP.style.overflowY = "scroll";
+  portfolioP.style.transitionDelay = "0.5s";
+  portfolioP.style.opacity = "1";
+  // portfolioButton.style.transitionDelay = "1.5s";
+  portfolioButton.style.opacity = "1";
+});
+
+portfolio.addEventListener("mouseout", function() {
+  portfolioP.style.transition = "0.5s";
+  portfolioP.classList.remove("fade-in");
+  portfolioP.classList.add("fade-out");
+  portfolioButton.style.transition = "0.5s";
+  portfolioButton.classList.remove("fade-in");
+  portfolioButton.classList.add("fade-out");
+  portfolioP.style.opacity = "0";
+  portfolioButton.style.opacity = "0";
+});
+
+contactDiv.addEventListener("mouseover", function() {
+  contactP.style.display = "block";
+  contactP.style.transition = "0.5s";
+  contactP.classList.remove("fade-out");
+  contactP.classList.add("fade-in");
+  contactButton.style.display = "block";
+  contactButton.style.transition = "0.5s";
+  contactButton.classList.remove("fade-out");
+  contactButton.classList.add("fade-in");
+  contactP.style.overflowY = "scroll";
+  contactP.style.transitionDelay = "0.5s";
+  contactP.style.opacity = "1";
+  // contactButton.style.transitionDelay = "1.5s";
+  contactButton.style.opacity = "1";
+});
+
+contact.addEventListener("mouseout", function() {
+  contactP.style.transition = "0.5s";
+  contactP.classList.remove("fade-in");
+  contactP.classList.add("fade-out");
+  contactButton.style.transition = "0.5s";
+  contactButton.classList.remove("fade-in");
+  contactButton.classList.add("fade-out");
+  contactP.style.opacity = "0";
+  contactButton.style.opacity = "0";
 });
